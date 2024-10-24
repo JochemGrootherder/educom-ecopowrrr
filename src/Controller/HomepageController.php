974d60fb39f63a19968f42041dd434ebae497146
Jochem Grootherder <J.grootherder@student.han.nl>
2024-10-24 12:39:29 +0200

    base project

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/')]
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    #[Route('/backhome', name: 'backhome')]
+    public function backHome()
+    {
+        return $this->redirectToRoute('homepage');
+    }
+}
